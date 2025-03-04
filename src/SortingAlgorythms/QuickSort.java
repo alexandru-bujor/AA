ea4f8280ba44f8bcae1790df1022573ed5fa190f
Bujor Alexandru <alexandru.bujor@isa.utm.md>
2025-03-04 12:09:06 +0200

    QuickSort implementation. Base case.

---
+++ src/SortingAlgorythms/QuickSort.java
@@ -0,0 +1,24 @@
+package SortingAlgorythms;
+import java.util.Arrays;
+
+
+public class QuickSort {
+
+
+
+    public static void main(String[] args) {
+
+            int[] array = {5, 6, 7, 3, 4, 61, 12, 45};
+
+            quickSort(array, 0 ,array.length - 1);
+    }
+
+    private static void quickSort(int[] arr, int start, int end) {
+
+        if (end <= start) return; //base case
+    }
+
+}
+
+
+
